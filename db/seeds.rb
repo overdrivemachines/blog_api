06e81695a8fd5f9419da61b1cfa4f8adf0af291c
Dipen Chauhan <get.dipen@gmail.com>
2018-07-01 18:35:02 -0700

    Added articles and annotated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 0..9 do
+	a = Article.new
+	a.title = Faker::Company.name
+	a.content = Faker::Company.catch_phrase #+ "\n" + Faker::Lorem.paragraphs(rand(1..3))
+	a.save
+end
